025ee251ecc4bc62038c411f477c4548ba9a1bc4
rico <dev_assasin@localhost.localdomain>
2023-12-23 13:57:03 +0300

    fix some.

--- src/main/java/com/springSecurity/springSecurityV2/SpringSecurityV2Application.java
+++ src/main/java/com/springSecurity/springSecurityV2/SpringSecurityV2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityV2Application {
-
+//! some comments
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityV2Application.class, args);
 	}
